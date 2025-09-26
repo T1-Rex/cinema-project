8a441715e26a191f633d3a3bfa72c0420004d8e7
Кузнецов Влад <dembrteak@gmail.com>
2025-09-26 16:03:43 +0500

    адаптив

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,7 @@
 const schemeSvg = document.querySelector('.scheme-svg');
 const totalPriceTag = document.querySelector('.price-total')
+const menuButton = document.querySelector('.m-menu');
+const menu = document.querySelector('.menu');
 let cost = 500;
 let totalPrice = 0;
 
@@ -14,3 +16,8 @@ schemeSvg.addEventListener('click', (event) =>{
     }
 
 });
+
+menuButton.addEventListener('click', () => {
+    menu.classList.toggle('is-open');
+});
+
